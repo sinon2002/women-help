159e6659bd3bce972ffd7cdaf8f87f453b28e871
sinon2002 <153992175+sinon2002@users.noreply.github.com>
2026-03-11 07:58:00 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openSurvey(){
-document.getElementById("survey").style.display="flex";
-}
-
-function closeSurvey(){
-document.getElementById("survey").style.display="none";
-alert("Спасибо за ваш ответ ❤️");
-}
