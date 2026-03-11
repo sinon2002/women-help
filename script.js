fed53b828e78a7cdf2276cbf365be5cdeee6b3dd
sinon2002 <153992175+sinon2002@users.noreply.github.com>
2026-03-11 07:29:49 +0600

    Initial commit: добавлена верстка сайта

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSurvey(){
+document.getElementById("survey").style.display="flex";
+}
+
+function closeSurvey(){
+document.getElementById("survey").style.display="none";
+alert("Спасибо за ваш ответ ❤️");
+}
