59914eb684f0b9aded3f7b733361bfb23cad4f4d
sinon2002 <153992175+sinon2002@users.noreply.github.com>
2026-03-22 09:35:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -395,7 +395,25 @@ dropdown.style.display = "none"
 
 
 
+let slides = document.querySelectorAll(".slide")
+let currentSlide = 0
 
+function showSlide(index){
+  slides.forEach(slide => slide.classList.remove("active"))
+  slides[index].classList.add("active")
+}
+
+// авто переключение
+setInterval(() => {
+  currentSlide++
+
+  if(currentSlide >= slides.length){
+    currentSlide = 0
+  }
+
+  showSlide(currentSlide)
+
+}, 3000) // каждые 3 секунды
 
 
 
